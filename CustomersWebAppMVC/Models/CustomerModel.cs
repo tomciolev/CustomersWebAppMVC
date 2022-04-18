1eccce3ccc60d1943e6f7f0df9289040bf735f51
solarskit@student.agh.edu.pl <solarskit@student.agh.edu.pl>
2022-04-18 21:02:28 +0200

    CustomerModel added

---
+++ CustomersWebAppMVC/Models/CustomerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomersWebAppMVC.Models
+{
+    public class CustomerModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string VAT { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Creation date")]
+        public DateTime CreationDate { get; set; }
+        [DisplayName("Phone number")]
+        public string PhoneNumber { get; set; }
+        public string Street { get; set; }
+        [DisplayName("Street number")]
+        public int StreetNumber { get; set; }
+        [DisplayName("Postal code")]
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
